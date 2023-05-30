d0c8167b9fff76bcf1b689bcd1855ba86b6a73c8
miaciasMAC <miaciasullo@gmail.com>
2023-05-30 16:25:34 -0400

    conditionally render add/remove friend button and edit form on profile page

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -5,9 +5,10 @@ import { Row, Col, Space, Avatar, Divider, Radio, Tabs, Button, Card } from "ant
 import { UserOutlined } from '@ant-design/icons'
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_USER } from "../utils/queries";
-import { ADD_FRIEND, REMOVE_FRIEND } from "../utils/mutations";
+import { ADD_FRIEND, REMOVE_FRIEND, REMOVE_FAV_BREWERY } from "../utils/mutations";
 import { format_date, format_timestamp } from '../utils/formatters';
 import FriendsList from "../components/FriendsList";
+import { EditUserForm } from "../components/EditUserForm";
 import Auth from "../utils/auth";
 const ObjectId = require("bson-objectid");
 
@@ -15,13 +16,20 @@ export function ProfilePage() {
   // const navigate = useNavigate();
   const { username } = useParams();
   const [profileData, setProfileData] = useState(null);
+  const [breweryList, setBreweryList] = useState(new Set([]));
+  const [showForm, setShowForm] = useState(false);
   const { loading, error, data: userData, refetch } = useQuery(GET_USER, {
     variables: { username },
   });
   const [addFriend] = useMutation(ADD_FRIEND);
   const [removeFriend] = useMutation(REMOVE_FRIEND);
+  const [removeFavBrewery] = useMutation(REMOVE_FAV_BREWERY);
+
   // gets loggedIn user's ID
-  // const { _id } = useUserContext();
+  const myData = useUserContext();
+  // console.log(Auth.getProfile())
+  const myId = Auth.getProfile()?.data?._id;
+
 
 
   // sets page data from URL and DB
@@ -64,6 +72,26 @@ export function ProfilePage() {
     }
   };
 
+  // removes favorite brewery
+  const handleRemoveFavBrewery = async (breweryId) => {
+    try {
+      const { data } = await removeFavBrewery({
+        variables: {
+          breweryId: breweryId,
+        },
+      });
+      setBreweryList((current) => {
+        // Create a new set of breweries excluding the deleted brewery
+        const updatedBreweries = new Set(
+          [...current].filter((brewery) => brewery.id !== breweryId)
+        );
+        return updatedBreweries;
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
 
   // custom avatar: Ant Design UI v5.4 does not support built-in avatars from URL
   const AvatarFromURL = ({ url, ...props }) => {
@@ -72,7 +100,7 @@ export function ProfilePage() {
     );
   };
   
-
+  // renders user friends, favorites, and wish lists
   const tabItems = [
     {
       label: `Friends (${profileData?.friendCount})`,
@@ -85,7 +113,7 @@ export function ProfilePage() {
       children: 'favorites stuff'
     },
     {
-      label: 'Wish List',
+      label: 'Wish List!',
       key: 3,
       children: 'some wishes'
     },
@@ -106,6 +134,23 @@ export function ProfilePage() {
                 <h2 style={{fontSize: '24px'}}>{userData.user.username}</h2>
                 <h3 style={{fontSize: '20px'}}>{userData.user.pronouns}</h3>
                 <p>{userData.user.bio}</p>
+                {/* show edit form if logged in User ID matches profile page ID */}
+                {myId === profileData?._id && (
+                  <Button onClick={() => setShowForm(!showForm)}>
+                    {showForm ? "Close" : "Edit Profile"}
+                  </Button>
+                )}
+                {/* consider replacing in-line edit form with modal form */}
+                {showForm && <EditUserForm />}
+                {myId !== profileData?._id && myData && (
+                  myData.user?.friends.some((friend) => friend.username === userData.username) ? (
+                    <Button onClick={() => handleRemoveFriend(userData._id)}>
+                      Remove Friend
+                    </Button>
+                  ) : (
+                    <Button onClick={handleFollowFriend}>Add Friend</Button>
+                  )
+                )}
               </Space>
             </Space>
             <Divider orientation='center' plain>Joined {format_date(userData.user?.createdAt)}</Divider>
