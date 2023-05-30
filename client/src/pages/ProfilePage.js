e8d7abf433c49b530e160eea9ed15cfe3ad4072d
miaciasMAC <miaciasullo@gmail.com>
2023-05-30 15:50:01 -0400

    added useEffect to profile page to handle user data of given profile. protected profile with Auth.loggedIn(). attempting to populate friends list in tab

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React , { useEffect, useState } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { useUserContext } from '../components/UserProvider';
-import { Row, Col, Space, Avatar, Divider, Button, Card } from "antd";
+import { Row, Col, Space, Avatar, Divider, Radio, Tabs, Button, Card } from "antd";
 import { UserOutlined } from '@ant-design/icons'
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_USER } from "../utils/queries";
 import { ADD_FRIEND, REMOVE_FRIEND } from "../utils/mutations";
 import { format_date, format_timestamp } from '../utils/formatters';
+import FriendsList from "../components/FriendsList";
+import Auth from "../utils/auth";
 const ObjectId = require("bson-objectid");
 
 export function ProfilePage() {
-
-  const [addFriend] = useMutation(ADD_FRIEND);
-  const [removeFriend] = useMutation(REMOVE_FRIEND);
   // const navigate = useNavigate();
   const { username } = useParams();
-  // const myData = useUserContext();
-  const { loading, error, data: userData } = useQuery(GET_USER, {
+  const [profileData, setProfileData] = useState(null);
+  const { loading, error, data: userData, refetch } = useQuery(GET_USER, {
     variables: { username },
   });
+  const [addFriend] = useMutation(ADD_FRIEND);
+  const [removeFriend] = useMutation(REMOVE_FRIEND);
+  // gets loggedIn user's ID
+  // const { _id } = useUserContext();
+
+
+  // sets page data from URL and DB
+  useEffect(() => {
+    if (!loading && userData.user !== null) {
+      setProfileData(userData.user);
+    }
+    refetch();
+  }, [loading, error, userData]);
+
 
   const handleFollowFriend = async () => {
     try {
@@ -47,9 +60,10 @@ export function ProfilePage() {
       // refetch();
       // navigate("/profile");
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
   };
+
 
   // custom avatar: Ant Design UI v5.4 does not support built-in avatars from URL
   const AvatarFromURL = ({ url, ...props }) => {
@@ -57,30 +71,55 @@ export function ProfilePage() {
       <Avatar {...props} src={url} />
     );
   };
+  
 
-  return (
-    <>
-      {userData?.user && !loading && (
-        <>
-          <Space direction='horizontal' size={16}>
-            {userData.user?.profilePic ? (
-              <AvatarFromURL url={userData.user?.profilePic} size={300} />
-            ) : (
-              <Avatar icon={<UserOutlined />} size={300} />
-            )}
-            <Space direction='vertical'>
-              <h2 style={{fontSize: '24px'}}>{userData.user.username}</h2>
-              <h3 style={{fontSize: '20px'}}>{userData.user.pronouns}</h3>
-              <p>{userData.user.bio}</p>
+  const tabItems = [
+    {
+      label: `Friends (${profileData?.friendCount})`,
+      key: 1,
+      children: 'some friends'
+    },
+    {
+      label: 'Favorites!',
+      key: 2,
+      children: 'favorites stuff'
+    },
+    {
+      label: 'Wish List',
+      key: 3,
+      children: 'some wishes'
+    },
+  ];
+
+  if (Auth.loggedIn()) {
+    return (
+      <>
+        {userData?.user && !loading && (
+          <>
+            <Space direction='horizontal' size={16}>
+              {userData.user?.profilePic ? (
+                <AvatarFromURL url={userData.user?.profilePic} size={300} />
+              ) : (
+                <Avatar icon={<UserOutlined />} size={300} />
+              )}
+              <Space direction='vertical'>
+                <h2 style={{fontSize: '24px'}}>{userData.user.username}</h2>
+                <h3 style={{fontSize: '20px'}}>{userData.user.pronouns}</h3>
+                <p>{userData.user.bio}</p>
+              </Space>
             </Space>
-          </Space>
-          <Divider orientation='center' plain>Joined {format_date(userData.user?.createdAt)}</Divider>
-          <Row>
-              
-          </Row>
-        </>
-      )}
-    </>
-  )
-
+            <Divider orientation='center' plain>Joined {format_date(userData.user?.createdAt)}</Divider>
+            <Tabs
+              defaultActiveKey="1"
+              size={12}
+              style={{marginBottom: 32}}
+              items={tabItems}
+            />
+          </>
+        )}
+      </>
+    )
+  } else {
+    return <h2>Please log in!</h2>;
+  }
 }
