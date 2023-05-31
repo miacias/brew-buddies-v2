4cf97853c31199b7216d7abf9d53650ff29b926e
miaciasMAC <miaciasullo@gmail.com>
2023-05-31 11:00:21 -0400

    refined friends list to paginate max 3 friends per page. also hides pagination menu when the user does not follow any accounts

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, List } from 'antd';
+import { Avatar, List, Pagination } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { ReactComponent as EmptyBin } from './EmptyBin.svg';
@@ -19,14 +19,22 @@ export default function FriendsList(friendsData) {
         <>
             {friendsData?.friends ? (
                 <List
+                    pagination={friendsData.friends.length ? {
+                        position: 'bottom',
+                        align: 'center',
+                        defaultCurrent: 1,
+                        pageSize: 3
+                    } : ''}
                     itemLayout="horizontal"
                     dataSource={friendsData.friends}
-                    locale={{emptyText: <div><EmptyBin/><p>Friends list empty</p></div>}}
+                    locale={{emptyText: <div><EmptyBin/><p>{`Not following anyone yet`}</p></div>}}
                     renderItem={(item, index) => (
                     <List.Item>
                         <List.Item.Meta
                         key={item._id}
-                        avatar={item.profilePic ? <AvatarFromURL url={item.profilePic} /> : <Avatar icon={<UserOutlined />} />}
+                        avatar={item.profilePic
+                            ? <Link to={`/profile/${item.username}`}><AvatarFromURL url={item.profilePic} /></Link> 
+                            : <Link to={`/profile/${item.username}`}><Avatar icon={<UserOutlined />} /></Link>}
                         title={<Link to={`/profile/${item.username}`}>{item.username}</Link>}
                         description={item.bio ? `${item.bio} Reviews: ${item.reviewCount}` : `Reviews: ${item.reviewCount}`}
                         />
